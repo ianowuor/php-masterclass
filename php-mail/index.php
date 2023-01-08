e508742a5f6d4e7f2b4ddd6922b0ad68b0b3d81f
Owuor Ian <owuorian95@gmail.com>
2023-01-08 19:07:31 +0300

    Added styling to the mail app

--- php-mail/index.php
+++ php-mail/index.php
@@ -2,22 +2,29 @@
 <html>
     <head>
         <title>Sending Emails With PHP</title>
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin+Sans">
     </head>
     <body>
-        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-            <label for='name'>Name: </label><br>
-            <input type="text" name="name" id="name"><br>
-            <label for='receiver'>To: </label><br>
-            <input type="email" name="receiver" id="receiver"><br>
-            <label for='subject'>Subject: </label><br>
-            <input type="text" name="subject" id="subject"><br>
-            <label for='emailBody'>Message: </label><br>
-            <textarea name="emailBody" id="emailBody"></textarea><br>
-            <input type="submit" value="Send" name="submit" id="submit">
-        </form>
+        <div id="heading">
+            <h1>Php Mail</h1>
+        </div>
+        <div id="formWrapper">
+            <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+                <!-- <label for='name'>Name: </label> -->
+                <input type="text" name="firstname" id="firstname" placeholder="First name" size="15"> 
+                <!-- <label for='receiver'>To: </label> -->
+                <input type="text" name="lastname" id="lastname" placeholder="Last name" size="15">
+                <input type="email" name="receiver" id="receiver" placeholder="Receiver's email" size="40">
+                <!-- <label for='subject'>Subject: </label> -->
+                <input type="text" name="subject" id="subject" placeholder="Subject" size="40">
+                <!-- <label for='emailBody'>Message: </label> -->
+                <textarea name="emailBody" id="emailBody" placeholder="Message" cols="55"></textarea>
+                <input type="submit" value="Send" name="submit" id="submit">
+            </form>
+        </div>
+        
         <?php
-            
-            
             if (isset($_POST['submit'])) {
                 $name = $_POST['name'];
                 $receiver = $_POST['receiver'];
@@ -35,8 +42,6 @@
                     header("Location: error.php");
                 }
             }
-
-
         ?>
     </body>
 </html>
