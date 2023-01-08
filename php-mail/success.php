cd71ab10e9894363af1b8411bb8c67fde2cfd953
Owuor Ian <owuorian95@gmail.com>
2023-01-08 21:32:51 +0300

    Success page for email application with styling

--- php-mail/success.php
+++ php-mail/success.php
@@ -1,2 +1,16 @@
-<p style="color: green;">Email sent successfully</p>
-<a href="index.php">Back</a>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Success Page</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin+Sans">
+</head>
+<body>
+    <div class="messageDiv">
+        <p id="success">Email sent successfully</p>
+    </div>
+    <div class="linkDiv">
+        <a href="index.php">Back</a>
+    </div>
+</body>
+</html>
