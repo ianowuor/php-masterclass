56729d28a0509e2754e0dbaae96285146f9a3ee4
Owuor Ian <owuorian95@gmail.com>
2023-01-08 21:29:15 +0300

    Php error page with styling

--- php-mail/error.php
+++ php-mail/error.php
@@ -1,2 +1,15 @@
-<p style="color: red;">Error sending email</p>
-<a href="index.php">Back</a>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="messageDiv">
+        <p id="error">Error sending email</p>
+    </div>
+    <div class="linkDiv">
+        <a href="index.php">Back</a>
+    </div>
+</body>
+</html>
