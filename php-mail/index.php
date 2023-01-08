12d9a05372097da7e72091fc1a6ed4e04c80e1a9
Owuor Ian <owuorian95@gmail.com>
2023-01-08 21:30:35 +0300

    Php email application with styling

--- php-mail/index.php
+++ php-mail/index.php
@@ -7,14 +7,16 @@
     </head>
     <body>
         <div id="heading">
-            <h1>Php Mail</h1>
+            <h1><span id="php">Php</span> <span id="mail">Mail</span></h1>
         </div>
         <div id="formWrapper">
             <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
                 <!-- <label for='name'>Name: </label> -->
-                <input type="text" name="firstname" id="firstname" placeholder="First name" size="15"> 
-                <!-- <label for='receiver'>To: </label> -->
-                <input type="text" name="lastname" id="lastname" placeholder="Last name" size="15">
+                <div id="nameWrapper">
+                    <input type="text" name="firstname" id="firstname" placeholder="First name" size="15"> 
+                    <!-- <label for='receiver'>To: </label> -->
+                    <input type="text" name="lastname" id="lastname" placeholder="Last name" size="15">
+                </div>
                 <input type="email" name="receiver" id="receiver" placeholder="Receiver's email" size="40">
                 <!-- <label for='subject'>Subject: </label> -->
                 <input type="text" name="subject" id="subject" placeholder="Subject" size="40">
@@ -23,16 +25,16 @@
                 <input type="submit" value="Send" name="submit" id="submit">
             </form>
         </div>
-        
         <?php
             if (isset($_POST['submit'])) {
-                $name = $_POST['name'];
+                $firstname = $_POST['firstname'];
+                $lastname = $_POST['lastname'];
                 $receiver = $_POST['receiver'];
                 $subject = $_POST['subject'];
                 $body = $_POST['emailBody'];
                 $sender = "From:owuorian95@gmail.com";
-                $message = $body . "\nFrom: ${name}";
-                if (empty($name) || empty($receiver) || empty($subject) || empty($body)) {
+                $message = $body . "\nFrom: ${firstname} ${lastname}";
+                if (empty($firstname) || empty($_POST['lastname']) || empty($receiver) || empty($subject) || empty($body)) {
                     die("<p style='color: red;'>All fields are mandatory</p>");
                 }
 
